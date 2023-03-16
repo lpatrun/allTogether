629f95c08f4c87d63d0ce3f8c935a0cbc17bae79
Krunoslav Blažičević <123366059+noKru1986@users.noreply.github.com>
2023-03-16 19:43:55 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import BlogPage from "./pages/BlogPage";
+import ContactPage from "./pages/ContactPage";
+import Error404Page from "./pages/Error404Page";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
---
+++ src/pages/HomePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Navigation } from "react-router-dom";
+function HomePage() {
+  return <nav>HomePage</nav>;
+}
+
+export default HomePage;
